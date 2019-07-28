4a0f06b4fa6b33db07b568f05b996a596efc6382
Sandhya Bagalkotkar <sandhyabagalkotkar@107-144-156-227.biz.spectrum.com>
2019-07-28 12:55:48 -0400

    cleaning up files

--- ersAPI/src/userconverter.ts
+++
@@ -1,5 +0,0 @@
-import User from '../models/user';
-
-export function convertSqlUser(row: any) {
-    return new User(row.userid, row.username, row.pass, row.firstname, row.lastname, row.email, row.roleid);
-}
